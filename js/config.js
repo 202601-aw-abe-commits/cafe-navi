2e6f5df6056989f74fc6ad1410f55148d010e2cc
abetakamasa <202601_aw_abe@x-ide.sakura.ne.jp>
2026-05-14 16:10:30 +0900

    fix(config): point github pages api base to deployed render url

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
   var isGitHubPages = host.indexOf('github.io') !== -1;
 
   // Replace with your actual Render API URL after deploy.
-  var renderApiBase = 'https://cafe-navi-api.onrender.com';
+  var renderApiBase = 'https://cafe-navi.onrender.com';
 
   window.CAFE_NAVI_API_BASE = isGitHubPages ? renderApiBase : '';
 })();
